9047f8f0d5dfa5cb414b7dad91678143fce050d0
Mario Lasso <prof.mario.lasso-mesa@iesalixar.org>
2025-01-13 00:04:46 +0100

    uso de /home/mario en ruta script crontab

--- scripts/cron_job.sh
+++ scripts/cron_job.sh
@@ -1,2 +1,2 @@
-docker compose -f /home/mario/hardening_apache_ssl/docker-compose.yml up certbot
-docker compose -f /home/mario/hardening_apache_ssl/docker-compose.yml exec webserver apachectl -k graceful
+docker compose -f $HOME/hardening_apache_ssl/docker-compose.yml up certbot
+docker compose -f $HOME/hardening_apache_ssl/docker-compose.yml exec webserver apachectl -k graceful
